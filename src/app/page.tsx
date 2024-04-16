20cfeb4b75f32830070adede77db810f8b3136ed
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-04-16 20:31:31 +0100

    auth home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,39 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { desc } from "drizzle-orm";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic"
 
+async function Images(){
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  });
+  return(
+    <div className="flex flex-wrap gap-4">
+          {images.map((image) => (
+            <div key={image.id} className="w-48 flex flex-col">
+              <img
+                src={image.url}
+                alt={image.name}
+              />
+              <div>{image.name}</div>
+            </div>
+          ))}
+        </div>
+  )
+}
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy:(model,{desc})=>desc(model.id)
-  });
-  
+
+
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="w-48 flex flex-col">
-            <img
-              src={image.url}
-              alt={image.name}
-            />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
     </main>
   );
 }
